494204ca02198d3bdefd838c17c0436098cd88c8
Marius Rusu <mariusrusu208@gmail.com>
2024-11-23 16:11:02 +0200

    added implementation for reading from file and outputing as file

--- Day1SAPLibProj/ex31_SHA1.cpp
+++ Day1SAPLibProj/ex31_SHA1.cpp
@@ -1,46 +1,49 @@
-#include<stdio.h>
-#include<malloc.h>
-#include<openssl/sha.h>
-
-#define INPUT_BLOCK_LENGTH 15
-
-int main() {
-
-	unsigned char input[] = { 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
-							  0x11,0x02,0x03,0x04,0x55,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
-							  0x21,0x02,0x03,0x04,0x65,0x06,0x07,0x08,0x09,0xAA,0x0B,0x0C,0xDD,0x0E,0x0F,
-							  0x01,0x02,0x03,0x04,0x75,0x06,0x07,0x08,0x09,0xBA,0x0B,0x0C,0xDD,0x0E };
-
-	SHA_CTX ctx;
-	SHA1_Init(&ctx);
-
-	//be careful, the char type can store values up to 256; it isn't an issue here with a small hardcoded value, but it is not recommend for larger files
-	unsigned char inputLength=sizeof(input) / sizeof(unsigned char); // total length of byteArray input
-	unsigned char remainingLength = inputLength;
-
-	while (remainingLength > 0) {
-
-		if (remainingLength > INPUT_BLOCK_LENGTH) {
-			//sha1 update done for 15-byte input
-			SHA1_Update(&ctx,&(input[inputLength-remainingLength]), INPUT_BLOCK_LENGTH);
-			remainingLength -= INPUT_BLOCK_LENGTH;
-		}
-		else {
-			//sha1 update done for less than 15 bytes
-			SHA1_Update(&ctx, &(input[inputLength - remainingLength]), remainingLength);
-			remainingLength = 0; //instruction for exiting from the while
-		}
-	}
-
-	unsigned char finalDigest[SHA_DIGEST_LENGTH];
-	SHA1_Final(finalDigest, &ctx); //this call also returns a int value for error codes
-
-	printf("\n\nSHA1 = ");
-	for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
-		printf("%02X ", finalDigest[i]);
-		printf(" ");
-	}
-	printf(" \n\n");
-	return 0;
-}
-
+//#include<stdio.h>
+//#include<malloc.h>
+//#include<openssl/sha.h>
+//
+//#define INPUT_BLOCK_LENGTH 15
+//
+//int main() {
+//
+//	unsigned char input[] = { 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
+//							  0x11,0x02,0x03,0x04,0x55,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
+//							  0x21,0x02,0x03,0x04,0x65,0x06,0x07,0x08,0x09,0xAA,0x0B,0x0C,0xDD,0x0E,0x0F,
+//							  0x01,0x02,0x03,0x04,0x75,0x06,0x07,0x08,0x09,0xBA,0x0B,0x0C,0xDD,0x0E };
+//	for (int i = 0;i < 59;i++) {
+//		printf("%02X", input[i]);
+//	}
+//
+//	SHA_CTX ctx;
+//	SHA1_Init(&ctx);
+//
+//	//be careful, the char type can store values up to 256; it isn't an issue here with a small hardcoded value, but it is not recommend for larger files
+//	unsigned char inputLength=sizeof(input) / sizeof(unsigned char); // total length of byteArray input
+//	unsigned char remainingLength = inputLength;
+//
+//	while (remainingLength > 0) {
+//
+//		if (remainingLength > INPUT_BLOCK_LENGTH) {
+//			//sha1 update done for 15-byte input
+//			SHA1_Update(&ctx,&(input[inputLength-remainingLength]), INPUT_BLOCK_LENGTH);
+//			remainingLength -= INPUT_BLOCK_LENGTH;
+//		}
+//		else {
+//			//sha1 update done for less than 15 bytes
+//			SHA1_Update(&ctx, &(input[inputLength - remainingLength]), remainingLength);
+//			remainingLength = 0; //instruction for exiting from the while
+//		}
+//	}
+//
+//	unsigned char finalDigest[SHA_DIGEST_LENGTH];
+//	SHA1_Final(finalDigest, &ctx); //this call also returns a int value for error codes
+//
+//	printf("\n\nSHA1 = ");
+//	for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
+//		printf("%02X ", finalDigest[i]);
+//		printf(" ");
+//	}
+//	printf(" \n\n");
+//	return 0;
+//}
+//
---
+++ Day1SAPLibProj/ex32_SHA1_fromFile.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<openssl/sha.h>
+
+#define INPUT_BLOCK_LENGTH 15
+
+int main() {
+	FILE* inputFile = NULL;
+	errno_t err;
+
+	err = fopen_s(&inputFile, "input_SHA1.bin", "rb");
+	if (err == 0) {
+		SHA_CTX ctx;
+		SHA1_Init(&ctx);
+
+		fseek(inputFile, 0, SEEK_END);
+		int fileLen = ftell(inputFile);
+		fseek(inputFile, 0, SEEK_SET);
+		
+		unsigned char* input = (unsigned char*)malloc(INPUT_BLOCK_LENGTH);
+		
+		int read_length = INPUT_BLOCK_LENGTH;
+		while (read_length == INPUT_BLOCK_LENGTH) {
+			read_length = fread(input, 1, INPUT_BLOCK_LENGTH, inputFile);
+			if (read_length > 0) {
+				SHA1_Update(&ctx, input, read_length);
+			}
+		}
+
+		unsigned char finalDigest[SHA_DIGEST_LENGTH];
+		SHA1_Final(finalDigest, &ctx); //this call also returns a int value for error codes
+
+		printf("\n\nSHA1 = ");
+		for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
+			printf("%02X ", finalDigest[i]);
+			printf(" ");
+		}
+		printf(" \n\n");
+		
+		FILE* outputFile = NULL;
+		err = fopen_s(&outputFile, "outputFile.txt", "w+");
+		if (err == 0) {
+			fprintf(outputFile,"\nSHA1 = ");
+			for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
+				fprintf(outputFile,"%02X ", finalDigest[i]);
+				fprintf(outputFile," ");
+			}
+			fclose(outputFile);
+		}
+
+		fclose(inputFile);
+	}
+	return 0;
+}
+
